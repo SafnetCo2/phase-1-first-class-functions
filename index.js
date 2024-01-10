dacd9eea671f732347791e19c20d78a3e54d13aa
nzi19001@byui.edu <nzi19001@byui.edu>
2024-01-10 21:42:09 +0300

    done

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ function receivesAFunction(callback) {
 function returnsANamedFunction() {
     return function namedFunction() {
         console.log("this is a named function");
+        //take no arguments
+        //return a named function
     }
 
 
 
 }
 function returnsAnAnonymousFunction() {
-    return () => console.log('this is anonymous function');
+    return () => console.log('this is anonymous function which has no name');
 }
